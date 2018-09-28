7659cc3a9969cbaa7d163cea6b5b7914a083cd56
Colin Kennedy <colinvfx@gmail.com>
2018-09-28 01:11:36 -0400

    strategy.py - Changed STRATEGIES to use an OrderedDict

--- build_adapters/strategy.py
+++ build_adapters/strategy.py
@@ -2,18 +2,50 @@
 # -*- coding: utf-8 -*-
 
 # IMPORT STANDARD LIBRARIES
-import copy
+import collections
+import os
 
 
-STRATEGIES = []
+class PrependOrderedDict(collections.OrderedDict):
+    def prepend(self, key, value, dict_setitem=dict.__setitem__):
+        # Reference: https://stackoverflow.com/questions/16664874
+        root = self._OrderedDict__root
+        first = root[1]
+
+        if key in self:
+            link = self._OrderedDict__map[key]
+            link_prev, link_next, _ = link
+            link_prev[1] = link_next
+            link_next[0] = link_prev
+            link[0] = root
+            link[1] = first
+            root[1] = first[0] = link
+        else:
+            root[1] = first[0] = self._OrderedDict__map[key] = [root, first, key]
+            dict_setitem(self, key, value)
+
+
+STRATEGIES = PrependOrderedDict()
 
 
 def get_strategies():
-    return copy.copy(STRATEGIES)
+    preference = os.getenv('REZZURECT_STRATEGY_ORDER', '')
+    order = []
+
+    if not preference:
+        return list(STRATEGIES.items())
+
+    for item in preference.split(','):
+        item = item.strip()
+
+        if item:
+            order.append(item)
+
+    return [(item_, STRATEGIES[item_]) for item_ in order]
 
 
 def register_strategy(name, function, priority=False):
     if priority:
-        STRATEGIES.insert(0, (name, function))
+        STRATEGIES.prepend(name, function)
     else:
-        STRATEGIES.append((name, function))
+        STRATEGIES[name] = function
