36b36d46a6db8e8e5e903d9f1e8e492bc36312c2
Colin Kennedy <colinvfx@gmail.com>
2018-09-30 20:28:15 -0400

    _environment.py - Changed init to not add imcomplete build methods

--- _environment.py
+++ _environment.py
@@ -137,8 +137,8 @@ def main(
 
     '''
     package_name = get_package_name(source_path)
-    add_from_internet(package_name, system, distribution, architecture)
+    # add_from_internet_build(package_name, system, distribution, architecture)
 
-    add_git_remote_ssh(install_path, system, distribution, architecture)
+    # add_git_remote_ssh_build(install_path, system, distribution, architecture)
 
-    add_git_remote_search(build_path, system.lower(), distribution, architecture)
+    # add_git_remote_build(build_path, system.lower(), distribution, architecture)
