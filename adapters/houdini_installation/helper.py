244f278960c4c4a5d3defdee72d5baeb9e0e4b22
Colin Kennedy <colinvfx@gmail.com>
2018-10-21 16:31:23 -0400

    WIP commit - Adding houdini support

---
+++ adapters/houdini_installation/helper.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Any information that is used between one or more adapter modules.'''
+
+
+def get_preinstalled_linux_executables(version):
+    '''Get a list of possible pre-installed executable Houdini files.
+
+    Args:
+        version (str): A version of Houdini. Example: "17.0.352".
+
+    Raises:
+        RuntimeError:
+            If we can't get version information from the stored version then
+            this function will fail. Normally though, assuming this adapter
+            was built correctly, this shouldn't occur.
+
+    Returns:
+        str: The absolute path to a Houdini executable.
+
+    '''
+    major, minor, patch = get_version_parts(version)
+
+    if not major:
+        raise RuntimeError(
+            'Version "{version}" has no major component. This should not happen.'
+            ''.format(version=version))
+
+    if not minor:
+        raise RuntimeError(
+            'Version "{version}" has no minor component. This should not happen.'
+            ''.format(version=version))
+
+    if not patch:
+        raise RuntimeError(
+            'Version "{version}" has no patch component. This should not happen.'
+            ''.format(version=version))
+
+    return {'/opt/hfs{version}/bin/houdini'.format(version=version), }
+
+
+def get_version_parts(text):
+    '''tuple[str, str, str]: Find the major, minor, and patch data of `text`.'''
+    return tuple(text.split('.'))
