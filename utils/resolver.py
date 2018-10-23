b05ee7f2712b3403e1e7e8f90506f1d09cd24f97
Colin Kennedy <colinvfx@gmail.com>
2018-10-22 23:13:01 -0400

    resolver.py - WIP commit - Adding the ability to query shotgun for 'sg_respawn_keys' - which is a field that defines the root paths on-disk

--- utils/resolver.py
+++ utils/resolver.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # IMPORT STANDARD LIBRARIES
+import logging
 import os
 
 # IMPORT THIRD-PARTY LIBRARIES
@@ -10,14 +11,39 @@ import json
 import yaml
 
 
+LOGGER = logging.getLogger('rezzurect.config')
+
+
 # TODO : Add this function to a rez-related module
-def get_pipeline_configuration_root():
-    raise NotImplementedError('Need to write this')
+def get_current_pipeline_configuration():
+    from sgtk import pipelineconfig_factory
+    return pipelineconfig_factory.from_path(os.getenv('TANK_CURRENT_PC'))
 
 
-# TODO : Replace this with a rez-related module function
-_PIPELINE_CONFIGURATION_ROOT = get_pipeline_configuration_root()
-# _PIPELINE_CONFIGURATION_ROOT = rez_helper.get_pipeline_configuration_root()
+def get_current_pipeline_configuration_safe():
+    try:
+        return get_current_pipeline_configuration()
+    except ImportError:
+        return None
+
+
+def get_current_pipeline_configuration_root():
+    return get_current_pipeline_configuration().get_config_location()
+
+
+def get_current_pipeline_configuration_root_safe():
+    try:
+        return get_current_pipeline_configuration_root()
+    except ImportError:
+        return ''
+
+
+try:
+    # TODO : Replace this with a rez-related module function
+    _PIPELINE_CONFIGURATION_ROOT = get_current_pipeline_configuration_root_safe()
+except ImportError:
+    # This happens if a rez package was called from command-line with no Shotgun Context
+    _PIPELINE_CONFIGURATION_ROOT = ''
 
 
 def _read_setting_file(path):
@@ -30,6 +56,9 @@ def _read_setting_file(path):
             return yaml.safe_load(file_)
 
     known_exceptions = (
+        # If the file does not exist
+        IOError,
+
         # If the JSON has syntax issues
         ValueError,
 
@@ -39,7 +68,7 @@ def _read_setting_file(path):
 
     for loader in (_as_yaml, _as_json):
         try:
-            return loader(path)
+            return loader(path) or dict()
         except known_exceptions:
             pass
 
@@ -47,21 +76,58 @@ def _read_setting_file(path):
 
 
 def _read_settings_from_shotgun_field():
-    raise NotImplementedError('Need to write this, too')
+    from sgtk import authentication
+    authenticator = authentication.ShotgunAuthenticator()
+    user = authenticator.get_user()
+    sg = user.create_sg_connection()
+
+    try:
+        has_respawn_key_field = bool(sg.schema_field_read('PipelineConfiguration', 'sg_respawn_keys'))
+    except Exception:  # TODO : Replace with `shotgun.Fault`
+        has_respawn_key_field = False
+
+    if not has_respawn_key_field:
+        return dict()
+
+    configuration_id = get_current_pipeline_configuration().get_shotgun_id()
+    configuration = sg.find_one(
+        'PipelineConfiguration',
+        [['id', 'is', configuration_id]],
+        ['sg_respawn_keys'],
+    )
+
+    return json.loads(configuration.get('sg_respawn_keys', ''))
+
+
+def _read_settings_from_shotgun_field_safe():
+    try:
+        return _read_settings_from_shotgun_field()
+    except (ImportError, TypeError):
+        LOGGER.exception('Respawn Shotgun Field field was not found or has a syntax error.')
+
+        configuration = get_current_pipeline_configuration_safe()
+
+        if configuration:
+            LOGGER.debug(
+                'Configuration name "%s" and id "%s" has missing field.',
+                configuration.get_project_disk_name(),
+                configuration.get_shotgun_id(),
+            )
+        else:
+            LOGGER.debug('No configuration was found.')
+
+        return dict()
 
 
 def _read_all_respawnrc_settings():
     output = dict()
 
-    # TODO : Need to get Shotgun settings to find out where the Pipeline
-    #        Configuration root is
-    #
     configuration_setting_file = os.path.join(_PIPELINE_CONFIGURATION_ROOT, '.respawnrc')
 
-    if os.path.file(configuration_setting_file):
+    if os.path.isfile(configuration_setting_file):
         output.update(_read_setting_file(configuration_setting_file))
 
-    output.update(_read_settings_from_shotgun_field())
+    output.update(_read_settings_from_shotgun_field_safe())
     output.update(_read_setting_file(os.path.expanduser('~/.respawnrc')))
 
     return output
@@ -71,4 +137,9 @@ def expand(text):
     settings = _read_all_respawnrc_settings()
     settings['respawn_root'] = _PIPELINE_CONFIGURATION_ROOT
 
-    return text.format(**settings)
+    try:
+        return text.format(**settings)
+    except KeyError:
+        LOGGER.exception(
+            'Text "%s" is missing one or more keys. Keys found, "%s".', text, settings)
+        raise
