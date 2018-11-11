152cfe68dc3eff2ccdebe66bf043532b167f540f
Colin Kennedy <colinvfx@gmail.com>
2018-11-11 14:29:38 -0500

    rezzurect_config.init_custom_pythonpath - Changed the function to prefer a 'REZZURECT_LOCATION' environment variable if it can be found

--- utils/rezzurect_config.py
+++ utils/rezzurect_config.py
@@ -27,12 +27,25 @@ REZ_PACKAGE_ROOT_FOLDER = _rezzurect_config.get_root_package_folder()
 
 
 def init_custom_pythonpath():
-    '''Add paths that Respawn needs to import packages to the user's session.'''
+    '''Add paths that Respawn needs to import packages to the user's session.
+
+    Important:
+        If the user has already defined the `REZZURECT_LOCATION`
+        environment variable then we will just use that, instead.
+
+    '''
     current_dir = os.path.dirname(os.path.realpath(__file__))
-
     vendors_directory = os.path.dirname(os.path.dirname(current_dir))
-    rez_python_package_directory = os.path.join(vendors_directory, 'rez-2.23.1-py2.7')
-    managed_paths = [vendors_directory, rez_python_package_directory]
 
-    sys.path.extend(managed_paths)
-    os.environ['RESPAWN_PYTHONPATH'] = (os.pathsep).join(managed_paths)
+    rezzurect_location = os.getenv('REZZURECT_LOCATION', '')
+
+    if not rezzurect_location:
+        rezzurect_location = vendors_directory
+
+    rez_python_package_directory = os.path.join(vendors_directory, 'rez-2.23.1-py2.7')
+
+    sys.path.append(rezzurect_location)
+    sys.path.append(rez_python_package_directory)
+
+    os.environ['REZZURECT_LOCATION'] = rezzurect_location
+    os.environ['RESPAWN_PYTHONPATH'] = rez_python_package_directory
